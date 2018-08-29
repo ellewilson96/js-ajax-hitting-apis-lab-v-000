a7ce2ad90ef2546afda74c91522cd6b912b6534c
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-29 20:04:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,53 +1,69 @@
 const rootURL = "https://api.github.com"
-
-function getRepositories() {
-  const name = document.getElementById("username").value
-  const uri = rootURL + "/users/" + name + "/repos"
-  const xhr = new XMLHttpRequest()
-  xhr.addEventListener("load", displayRepositories)
-  xhr.open("GET", uri)
-  xhr.send()
+ function getRepositories() {
+  const name = document.getElementById("username").value;
+  const address = rootURL + "/users/" + name + "/repos";
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", displayRepositories)
+  req.open("GET", address)
+  req.send()
   return false;
 }
-function displayRepositories() {
-  const repos = JSON.parse(this.responseText)
-  const repoList = "<ul>" + repos.map(repo => {
-    const dataUsername = 'data-username="' + repo.owner.login + '"'
-    const dataRepoName = 'data-repository="' + repo.name + '"'
-    return(`
-          <li>
-            <h2>${repo.name}</h2>
-            <a href="${repo.html_url}">${repo.html_url}</a><br>
-            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
-            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
-          </li>`
-          )
-  }).join('') + "</ul>";
+ function displayRepositories() {
+  const repos = JSON.parse(this.responseText);
+  const repoList = repos.map(function(repo) {
+    const listUsername = repo.owner.login
+    const listRepoName = repo.name
+    return (`
+      <ul>
+        <li>
+          <h2>${repo.name}</h2>
+          <a href="${repo.html_url}">${repo.html_url}</a><br>
+          <a href="#" onclick="getCommits(this)">Get Commits</a><br>
+          <a href="#" onclick="getBranches(this)">Get Branches</a><br>
+        </li>
+      </ul>`
+    );
+  }).join("")
   document.getElementById("repositories").innerHTML = repoList
-}
-function getCommits(el) {
+};
+ function getCommits(el) {
   const repoName = el.dataset.repository
-  const uri = rootURL + "/repos/" + el.dataset.username + "/" + repoName + "/commits"
-  const xhr = new XMLHttpRequest()
-  xhr.addEventListener("load", displayCommits)
-  xhr.open("GET", uri)
-  xhr.send()
-}
-function displayCommits() {
+  const address = rootURL + "/repos/" + el.dataset.username + "/" + repoName + "/commits"
+  const req = new XMLHttpRequest()
+   req.addEventListener("load, displayCommits")
+  req.open("GET", address)
+  req.send()
+  return false;
+};
+ function displayCommits() {
   const commits = JSON.parse(this.responseText)
-  const commitsList = `<ul>${commits.map(commit => '<li><h3>' + commit.commit.author.name + ' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>').join('')}</ul>`
+  const commitsList = commits.map(function(commit) {
+    return (`
+      <ul>
+        <li>${commit.commit.author.name}</li>
+        <li>${commit.author.login}</li>
+        <li>${commit.commit.message}</li>
+      </ul>`
+    );
+  });
   document.getElementById("details").innerHTML = commitsList
-}
-function getBranches(el) {
+};
+ function getBranches(el) {
   const repoName = el.dataset.repository
-  const uri = rootURL + "/repos/" + el.dataset.username + "/" + repoName + "/branches"
-  const xhr = new XMLHttpRequest()
-  xhr.addEventListener("load", displayBranches)
-  xhr.open("GET", uri)
-  xhr.send()
-}
-function displayBranches() {
+  const address = rootURL + "/repos/" + el.dataset.username + "/" + repoName + "/branches"
+  const req = new XMLHttpRequest()
+   req.addEventListener("load", displayBranches)
+  req.open("GET", address)
+  req.send()
+};
+ function displayBranches() {
   const branches = JSON.parse(this.responseText)
-  const branchesList = `<ul>${branches.map(branch => '<li>' + branch.name + '</li>').join('')}</ul>`
+  const branchesList = branches.map(function(branch) {
+    return (`
+      <ul>
+        <li>${branch.name}</li>
+      </ul>`
+    );
+  });
   document.getElementById("details").innerHTML = branchesList
-}
+};
